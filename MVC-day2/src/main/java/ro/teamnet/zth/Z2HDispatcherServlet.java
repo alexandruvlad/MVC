485244b91cec4b47a23bbe1cbec92b09f42ed41c
Alexandru.Vlad <vlad.alex95@yahoo.com>
2017-07-24 16:54:37 +0300

    MVC

---
+++ MVC-day2/src/main/java/ro/teamnet/zth/Z2HDispatcherServlet.java
@@ -0,0 +1,93 @@
+package ro.teamnet.zth;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import ro.teamnet.zth.fmk.MethodAttributes;
+import ro.teamnet.zth.fmk.domain.HttpMethod;
+import ro.teamnet.zth.utils.BeanDeserializator;
+import ro.teamnet.zth.utils.ComponentScanner;
+import ro.teamnet.zth.utils.ControllerScanner;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class Z2HDispatcherServlet extends HttpServlet {
+    private ComponentScanner controllerScanner;
+
+
+    @Override
+    public void init() throws ServletException {
+        controllerScanner=new ControllerScanner("ro.teamnet.zth.appl.controller");
+        controllerScanner.scan();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        dispatchReply(req, resp, HttpMethod.GET);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        dispatchReply(req, resp, HttpMethod.POST);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        dispatchReply(req, resp, HttpMethod.DELETE);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        dispatchReply(req, resp, HttpMethod.PUT);
+    }
+
+    private void dispatchReply(HttpServletRequest req, HttpServletResponse resp, HttpMethod methodType) {
+        try {
+            Object resultToDisplay = dispatch(req, methodType);
+            reply(resp, resultToDisplay);
+        } catch (Exception e) {
+            try {
+                sendExceptionError(e, resp);
+            } catch (IOException ioe) {
+                throw new RuntimeException(ioe);
+            }
+        }
+    }
+
+    private void sendExceptionError(Exception e, HttpServletResponse resp) throws IOException {
+        resp.getWriter().print(e.getMessage());
+    }
+
+    private void reply(HttpServletResponse resp, Object resultToDisplay) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        final String responseAsString;
+        try {
+            responseAsString = objectMapper.writeValueAsString(resultToDisplay);
+            resp.getWriter().print(responseAsString);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private Object dispatch(HttpServletRequest req, HttpMethod methodType) {
+        String reqUrlPath=req.getPathInfo();
+        Object controllerInstance=controllerScanner.getInstance(reqUrlPath,methodType);
+        Method method=controllerScanner.getMethodMetaData(reqUrlPath,methodType).getMethod();
+        List<Object> params= BeanDeserializator.getMethodParams(method,req);
+        Object  ret;
+        try {
+            ret = method.invoke(controllerInstance,params.toArray());
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+        return ret;
+    }
+
+
+}
